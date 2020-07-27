300fe1332252b8094187862455320325bca82abd
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-27 12:53:28 -0700

    clean up index.html

--- pintereach/src/actions/index.js
+++ pintereach/src/actions/index.js
@@ -3,9 +3,10 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 export const FETCH_BOARDS_START = 'FETCH_BOARDS_START';
 export const FETCH_BOARDS_SUCCESS = 'FETCH_BOARDS_SUCCESS';
 export const FETCH_BOARDS_ERROR = 'FETCH_BOARDS_ERROR';
-export const NEW_POST = 'NEW_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const EDIT_POST = 'EDIT_POST';
+
+export const NEW_POST = 'NEW_BOARD';
+export const DELETE_BOARD = 'DELETE_BOARD';
+export const EDIT_BOARD = 'EDIT_BOARD';
 
 export const fetchBoards = () => {
   return (dispatch) => {
