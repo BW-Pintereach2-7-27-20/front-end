abf46af72b634775e49b49cb930cc71c90806521
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-27 14:09:29 -0700

    register and login basics down

--- pintereach/src/components/Login.js
+++ pintereach/src/components/Login.js
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-import styled from 'styled-components';
-import UsernameIcon from '../images/username-icon.png';
-import PassIcon from '../images/password-icon.png';
-import LoginIcon from '../images/login-icon.png';
-import Background from '../images/dummy-background.jpg';
-
 const initialValue = {
   username: '',
   password: '',
 };
 
-const Login = () => {
+const Login = (props) => {
   const [inputValue, setInputValue] = useState(initialValue);
 
   const onChange = (e) => {
@@ -23,7 +17,7 @@ const Login = () => {
     e.preventDefault();
 
     axiosWithAuth()
-      .post('/login')
+      .post('/login', inputValue)
       .then((res) => {
         localStorage.setItem('token', res.data.payload);
         props.history.push('/dashboard');
