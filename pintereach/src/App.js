36bebe17885a7f79729962402b218aa911444b35
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-27 09:48:57 -0700

    set up basic boilerplate for react router

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <Login></Login>
-    </div>
+    <Router>
+      <div className='App'>
+        <Route exact path='/register'>
+          <Register />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- pintereach/src/components/Login.js
+++ pintereach/src/components/Login.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Login() {
-  return <div>testing</div>;
-}
+const Login = () => {
+  return (
+    <>
+      <h2>Login here</h2>
+      <form>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' placeholder='john@doe.com' />
+        <label htmlFor='password'>Password</label>
+        <input type='text' name='password' placeholder='********' />
+
+        <button>Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
