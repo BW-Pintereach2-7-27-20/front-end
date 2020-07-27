53310226feadd32a76e4f338f643fc32cda946ea
Ashley <ashley.bergsma42@gmail.com>
2020-07-27 12:35:06 -0700

    Imported styled-components, put a CSS reset in the App.css file, set body disply to flex, centered with justify-content, added images folder to hold icons.

--- pintereach/src/components/Login.js
+++ pintereach/src/components/Login.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import styled from 'styled-components'; 
+import UsernameIcon from '../images/username-icon.png'; 
+import PassIcon from '../images/password-icon.png'; 
 
 const initialValue = {
   username: '',
@@ -16,11 +19,51 @@ const Login = () => {
     e.preventDefault();
   };
 
+  const StyledDiv = styled.div`
+  display: flex;
+  margin-top: 5%;
+  border: 1px solid red;
+  width: 80vw;
+  `;
+
+  const StyledHeader = styled.h2`
+    color: indianred;
+    font-weight: bold;
+    font-size: 3.0rem;
+    text-shadow: 2px 2px gray;
+    padding: 2%;
+    width: 20%;
+    align-self: center;
+    margin: 0 auto;
+  `;
+
+  const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    padding: 5%;
+    width: 50%;
+    border: 1px solid gray;
+
+    label {
+      font-size: 2.0rem;
+    }
+
+    input {
+      height: 5vh;
+      font-family: serif;
+    }
+
+    img {
+      height: 5vh;
+    }
+  `;
+
+
   return (
-    <>
-      <h2>Login here</h2>
-      <form onSubmit={onSubmit}>
-        <label htmlFor='username'>Username</label>
+    <StyledDiv>
+      <StyledHeader>Login here</StyledHeader>
+      <StyledForm onSubmit={onSubmit}>
+        <label htmlFor='username'><img src={UsernameIcon} alt='username icon'/> Username</label>
         <input
           type='text'
           name='username'
@@ -28,7 +71,7 @@ const Login = () => {
           value={inputValue.username}
           onChange={onChange}
         />
-        <label htmlFor='password'>Password</label>
+        <label htmlFor='password'><img src={PassIcon} alt='password icon' /> Password</label>
         <input
           type='text'
           name='password'
@@ -38,8 +81,8 @@ const Login = () => {
         />
 
         <button>Login</button>
-      </form>
-    </>
+      </StyledForm>
+    </StyledDiv>
   );
 };
 
