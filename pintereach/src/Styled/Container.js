a78a8ec5c4604a8c9faae3f1df8e24da88f4a31b
Ashley <ashley.bergsma42@gmail.com>
2020-07-27 16:36:11 -0700

    icons added to image folder

--- pintereach/src/Styled/Container.js
+++ pintereach/src/Styled/Container.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Container = styled.div`
     display: flex;
     justify-content: center;
-    border: 1px solid red;
     width: 80vw;
     height: 80vh;
     background-color: lavenderblush;
