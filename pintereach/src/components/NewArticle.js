ca1975721fbb13c1cbc71bf6ea6f45c79d1b32c9
Ashley <ashley.bergsma42@gmail.com>
2020-07-28 10:00:06 -0700

    new article has labels and inputs

--- pintereach/src/components/NewArticle.js
+++ pintereach/src/components/NewArticle.js
@@ -18,11 +18,43 @@ const NewArticle = () => {
         url: '',
     }
 
-    
+
 
     return (
         <div>
             New articles will go here! 
+            <h2>Add an Article</h2>
+            
+            <form className='articleForm'> 
+            
+            <label htmlFor='url'>url:</label>
+            <input 
+            type='text'
+            name='url'
+            placeholder='newarticle.com/info'
+            />
+
+            <label htmlFor='title'>Title:</label>
+            <input 
+            type='text'
+            name='title'
+            placeholder='article title'
+            />
+
+            <label htmlFor='author'>Author:</label>
+            <input 
+            type='text'
+            name='author'
+            placeholder='author name'
+            />
+
+            <label htmlFor='host'>Host:</label>
+            <input 
+            type='text'
+            name='host'
+            placeholder='host'
+            />
+            </form>
         </div>
     )
 }
