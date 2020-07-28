536c528a9693e761cec1e64c03e8b461ffea8ed0
Ashley <ashley.bergsma42@gmail.com>
2020-07-28 10:27:20 -0700

    added onChange and included in the input JSX. Added yup as dependency and a validation folder

--- pintereach/src/components/NewArticle.js
+++ pintereach/src/components/NewArticle.js
@@ -1,6 +1,4 @@
-import React from 'react'; 
-
-const NewArticle = () => { 
+import React, { useState } from 'react'; 
 
     // setting initial form state
     // should we add a checkbox form input to organize the new articles to an appropriate board? i.e. (category { research: false, personal: false, coding: false, })
@@ -18,7 +16,12 @@ const NewArticle = () => {
         url: '',
     }
 
+const NewArticle = (props) => { 
+    const [inputValue, setInputValue] = useState(initialNewArticle); 
 
+    const onChange = (e) => {
+        setInputValue({...inputValue, [e.target.name]: e.target.value});
+    }
 
     return (
         <div>
@@ -31,24 +34,28 @@ const NewArticle = () => {
             type='text'
             name='url'
             placeholder='newarticle.com/info'
+            onChange={onChange}
             />
             <label htmlFor='title'>Title:</label>
             <input 
             type='text'
             name='title'
             placeholder='article title'
+            onChange={onChange}
             />
             <label htmlFor='author'>Author:</label>
             <input 
             type='text'
             name='author'
             placeholder='author name'
+            onChange={onChange}
             />
             <label htmlFor='host'>Host:</label>
             <input 
             type='text'
             name='host'
             placeholder='host'
+            onChange={onChange}
             />
 
             </form>
