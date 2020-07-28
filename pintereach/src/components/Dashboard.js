457cd6f0dd782a3bd494bddaaa35811d799d4405
Ashley <ashley.bergsma42@gmail.com>
2020-07-28 16:38:25 -0700

    saving changes before pull

--- pintereach/src/components/Dashboard.js
+++ pintereach/src/components/Dashboard.js
@@ -2,6 +2,32 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchBoards } from '../actions';
+// styling imports
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  background-color: white;
+  font-family: 'Roboto Slab', serif;
+  border: 1px solid gray;
+  padding: 2%;
+  width: 85vw;
+  margin: 0 auto;
+`;
+
+const StyledDiv = styled.div`
+  border: 1px solid red;
+  height: 25vh;
+  width: 20vw;
+
+  h2 {
+    font-size: 2.8rem;
+    font-weight: bold;
+  }
+
+  img {
+    height: 10vh;
+  }
+`;
 
 const Dashboard = (props) => {
   useEffect(() => {
@@ -9,20 +35,21 @@ const Dashboard = (props) => {
   }, []);
 
   return (
-    <div>
-      <div className='board-wrapper'>
+    <StyledContainer>
+      <StyledDiv>
         <h2>Board Title</h2>
+        <img src='' alt='board thumbnail' />
+        {/* eventually enable a toggle to drop down ul of articles */}
         <ul>
           <li>
-            <img src='' alt='article thumbnail' />
             <p>Article title</p>
           </li>
         </ul>
-      </div>
+      </StyledDiv>
       <Link to='/new-board'>Create new Board</Link>
       {props.isLoading && <span>Loading Boards...</span>}
       {console.log(props.isLoading)}
-    </div>
+    </StyledContainer>
   );
 };
 
