b08895a27d69357a26b9f2c30d8639ccf57aee89
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-30 19:51:12 -0700

    work on update function

--- pintereach/src/components/Board.js
+++ pintereach/src/components/Board.js
@@ -1,32 +1,32 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
-import styled from 'styled-components'; 
-
-const StyledLink = styled(Link)`
-    color: indianred;
-    font-size: 1.2rem;
-    font-weight: bold;
-    font-family: 'Roboto Slab', serif;
-    text-shadow: 1px 1px 5px coral;
-    text-transform: capitalize;
-    text-decoration: none;
-
-    &:hover {
-    text-shadow: 1px 1px 5px white, 0px 0px 6px coral;
-    }
-`;
-
 import { connect } from 'react-redux';
 import { selectBoard } from '../actions';
+import styled from 'styled-components';
 
+const StyledLink = styled(Link)`
+  color: indianred;
+  font-size: 1.2rem;
+  font-weight: bold;
+  font-family: 'Roboto Slab', serif;
+  text-shadow: 1px 1px 5px coral;
+  text-transform: capitalize;
+  text-decoration: none;
 
+  &:hover {
+    text-shadow: 1px 1px 5px white, 0px 0px 6px coral;
+  }
+`;
 
 const Board = ({ board }) => {
   return (
     <div className='board-wrapper'>
-
-      <StyledLink to={`/board/${board.id}`} onClick={() => selectBoard(board.id)}>{board.name}</StyledLink>
+      <StyledLink
+        to={`/board/${board.id}`}
+        onClick={() => selectBoard(board.id)}
+      >
+        {board.name}
+      </StyledLink>
       <p>{board.description}</p>
     </div>
   );
